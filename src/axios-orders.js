23494407ee40b714726b3c52781d733324828904
AbelardoRivas <abelardo.rivas1@gmail.com>
2019-01-10 10:20:21 -0400

    Enviando petición a POST en BD Firebase

--- src/axios-orders.js
+++ src/axios-orders.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'https://react-my-burger-cee5b.firebaseio.com/'
+  baseURL: 'https://react-my-burger-cee5b.firebaseio.com/' //modificar con Firebase URL
 });
 
 export default instance;
