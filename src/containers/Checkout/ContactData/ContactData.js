684426bd599d8cca6d8f079afd2ac668de22d26b
AbelardoRivas <abelardo.rivas1@gmail.com>
2019-01-21 19:38:00 -0400

    Agregando un componente desplegable

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -92,7 +92,7 @@ class ContactData extends Component {
         {formElementsArray.map(formElement => (
           <Input
             key={formElement.id}
-            elementType={formElement.config.elememtType}
+            elementType={formElement.config.elementType}
             elementConfig={formElement.config.elementConfig}
             value={formElement.config.value} />
         ))}
