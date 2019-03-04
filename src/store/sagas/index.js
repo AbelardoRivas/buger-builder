63de8dd537abbf837f0da5acfa45ca708d9fbd0d
Abelardo Rivas <abelardo.rivas1@gmail.com>
2019-03-04 10:55:59 -0400

    Moviendo más lógica a las Sagas

--- src/store/sagas/index.js
+++ src/store/sagas/index.js
@@ -2,7 +2,9 @@ import { takeEvery } from 'redux-saga/effects';
 
 import * as actionTypes from '../actions/actionTypes';
 import { logoutSaga } from './auth';
+import { checkAuthTimeout } from '../actions/auth';
 
 export function* watchAuth() {
+    yield takeEvery(actionTypes.AUTH_CHECK_TIMEOUT, checkAuthTimeout);
     yield takeEvery(actionTypes.AUTH_INITIATE_LOGOUT, logoutSaga);
 }
